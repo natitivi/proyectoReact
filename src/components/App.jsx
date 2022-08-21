319ba7d3ad9bc33f112d0f48a73ad125de02e3a9
natividad <nati.orobles@gmail.com>
2022-08-20 22:57:34 -0500

    incorporacion de navbar

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Unnamed</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor02">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">Home
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Shop</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+    </div>
+  );
+}
+
+export default App;
